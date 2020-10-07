a37188fa0a3e8b01d57af0aeed9a5e3257237683
Ksanork <ksanork@gmail.com>
2020-10-07 12:12:01 +0200

    - add sensor to create streams with one-column csv file - add example pipeline file - add readme with simple_columns_sensor example  -

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # ssi-synchronization
+
+Repozytorium zawiera dodatkowe moduły pomocne przy synchronizacji strumieni danych za pomocą frameworka SSI (https://github.com/hcmlab/ssi)
+
+### Sensory:
+#### simple_columns_csv_sensor
+
+Pozwala na stworzenie strumienia liczb zmiennoprzecinkowych z daną częstotliwością z pliku `csv`
+Dotyczy jedynie plików, które składają się z jednej kolumny danych.
+
+Domyślny format zakłada, że pierwsza linia w pliku `csv` to data (_timestamp_) a druga to częstotliwość
+
+Plik `01_sensor.pipeline` pokazuje przykładowe wykorzystanie w pipeline.
+##### Przykład użycia:
+```xml
+<sensor create="PythonSensor" optsstr="path=HR.csv;headers_ln=2;sr=10.0" script="simple_columns_csv_sensor" block="0.1">
+    <output channel="csv_data" pin="hr" />
+</sensor>
+```
+
+##### Parametry:
+* `path` - scieżka do pliku `csv`, typ `STRING`
+* `sr` - częstotliwość (_sample rate_), typ `FLOAT`, jeżeli również `auto_init` ustawione jest jako `1` to brany pod uwagę jest paramatr `sr`, a nie wartość z pliku
+* `headers_ln` - ilość lini nagłówkowych na początku pliku, którę będą ignorowane podczas wczytywania danych do strumienia, typ `INTEGER`
+* `auto_init` - jeżeli ustawione jako `1` to skrypt stara się pobrać częstotliwość z linii nagłówkowych (na podstawie domyślnego formatu opisanego wyżej). Domyślnie ustawione jako `1`
+
+##### Przykładowy plik csv:
+```csv
+1600692252.000000
+100.000
+1.0
+2.0
+3.0
+4.0
+...
+```
---
+++ simple_columns_csv_sensor.py
@@ -0,0 +1,79 @@
+﻿import csv
+
+INITIAL_OFFSET = -200
+
+
+def read_from_file(vars, opts):
+    headers_ln = opts['headers_ln']
+    path = opts['path']
+    auto_init = opts['auto_init']
+
+    with open(path, 'r') as csvfile:
+        csv_reader = csv.reader(csvfile, delimiter=',')
+
+        if headers_ln >= 2 and auto_init == 1:
+            # date
+            vars['initial_timestamp'] = next(csv_reader)[0]
+
+            # rate
+            vars['sr'] = next(csv_reader)[0]
+        else:
+            for i in range(headers_ln):
+                next(csv_reader)
+
+        output = []
+        for row in csv_reader:
+            output.append(float(row[0]))
+
+        return output
+
+
+def getOptions(opts, vars):
+    opts['path'] = ''
+    opts['sr'] = -1.0
+    opts['name'] = 'diagram_channel'
+    opts['offset'] = 0
+    opts['headers_ln'] = 2
+    opts['auto_init'] = 1
+
+
+def getChannelNames(opts, vars):
+    return { 'csv_data' : '' }
+
+
+def initChannel(name, channel, types, opts, vars):
+    if opts['path'] == '':
+        print('[csv_sensor] empty path')
+
+    if name == 'csv_data':
+        vars['hr'] = read_from_file(vars, opts)
+
+        channel.dim = 1
+        channel.type = types.FLOAT
+        channel.sr = opts['sr'] if opts['sr'] != -1 else vars['sr']
+    else:
+        print('[csv_sensor] unkown channel name')
+
+
+def connect(opts, vars):
+    vars['pos'] = INITIAL_OFFSET + opts['offset']
+
+
+def read(name, sout, reset, board, opts, vars):
+    hr = vars['hr']
+    pos = vars['pos']
+
+    if name == 'csv_data':
+        for n in range(sout.num):
+            if pos < len(hr):
+                sout[n] = hr[pos]
+                pos += 1
+
+    else:
+        print('[csv_sensor] unkown channel name')
+
+    vars['pos'] = pos
+
+
+def disconnect(opts, vars):
+    pass
